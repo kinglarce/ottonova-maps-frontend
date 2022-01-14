a1bfc860828843e94031688b6c025213e8f7b33a
King Larce Cortes <kingcort01@gmail.com>
2022-01-14 08:41:42 +0800

    feat: Improve maps component and add stylings

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,24 @@
 import React, { ReactElement, FC } from 'react'
+import GoogleMapReact from 'google-map-react'
+import useStyles from './styles'
 
+const API_KEY = 'AIzaSyCOJHqWc6f69wJDwoyUuw7xl2JKcdqVMcA'
 const Map: FC = (): ReactElement => {
-  return <h1>Map</h1>
+  const classes = useStyles()
+  const coordinates = { lat: 0, lng: 0 }
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      >
+      </GoogleMapReact>
+    </div>
+  )
 }
 
 export default Map
