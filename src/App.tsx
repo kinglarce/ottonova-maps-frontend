9ceb97b77f67e9ffcddac2375ed68f01562c3049
King Larce Cortes <kingcort01@gmail.com>
2022-01-14 08:09:28 +0800

    feat: Add theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
-import { CssBaseline, Grid } from '@material-ui/core'
+import { CssBaseline, Grid, createTheme, ThemeProvider } from '@material-ui/core'
+import { lightBlue } from '@material-ui/core/colors'
 
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: lightBlue
+  }
+})
+
 function App () {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline>
         <Header title="Ottonova Tours" />
         <Grid container spacing={3} style={{ width: '100%' }}>
@@ -20,7 +30,7 @@ function App () {
           </Grid>
         </Grid>
       </CssBaseline>
-    </>
+    </ThemeProvider>
   )
 }
 
