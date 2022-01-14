2a2a5dd3c73a9ab96d5c351fbd443a58b68637ce
King Larce Cortes <kingcort01@gmail.com>
2022-01-14 09:21:24 +0800

    feat: Improve list component and add menu option for continents

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,37 @@
-import React, { ReactElement, FC } from 'react'
+import React, { ReactElement, FC, useState } from 'react'
+import {
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select
+} from '@material-ui/core'
+
+import useStyles from './styles'
 
 const List: FC = (): ReactElement => {
-  return <h1>List</h1>
+  const classes = useStyles()
+  const [continent, setContinent] = useState<string>('')
+  const continents: string[] = ['Europe', 'Australia']
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Landmarks Nearby</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Continents</InputLabel>
+        <Select
+          value={continent}
+          onChange={(e) => setContinent(e.target.value as string)}
+        >
+          {continents?.map((value: string, i) => (
+            <MenuItem key={i} value={value}>
+              {value}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  )
 }
 
 export default List
